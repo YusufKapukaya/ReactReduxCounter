9ccff2c755b2e26b19ab1754f2e81fcba7879e42
YusufKapukaya <62699767+YusufKapukaya@users.noreply.github.com>
2023-01-11 15:59:55 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import {Provider} from 'react-redux';
+
+const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <Provider store={store}>
+  <App />
+  </Provider>
+);
+
+
+reportWebVitals();
